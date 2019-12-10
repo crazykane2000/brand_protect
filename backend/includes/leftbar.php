bc14a7c11e75d7d0482b5e8419feca8b77adad32
unknown <crazykane2000@gmail.com>
2019-12-10 13:55:43 +0530

    first commit

---
+++ backend/includes/leftbar.php
@@ -0,0 +1,33 @@
+<!-- ========== Left Sidebar Start ========== -->
+<div class="left side-menu">
+    <div class="sidebar-inner slimscrollleft">
+
+        <!--- Sidemenu -->
+        <div id="sidebar-menu">
+            <ul>
+              
+                <li class="has_sub">
+                    <a href="dashboard.php" class="waves-effect"><i class="zmdi zmdi-view-dashboard"></i>
+                    <span> Brand Reg. Logs </span> </a>
+                </li>
+
+                 <li class="has_sub">
+                    <a href="view_business.php" class="waves-effect"><i class="zmdi zmdi-view-dashboard"></i>
+                    <span> Business Reg. Logs </span> </a>
+                </li>
+
+                 <li class="has_sub">
+                    <a href="view_brand.php" class="waves-effect"><i class="zmdi zmdi-view-dashboard"></i>
+                    <span> Brand Documents </span> </a>
+                </li>
+              
+            </ul>
+            <div class="clearfix"></div>
+        </div>
+        <!-- Sidebar -->
+        <div class="clearfix"></div>
+
+    </div>
+
+</div>
+<!-- Left Sidebar End -->
